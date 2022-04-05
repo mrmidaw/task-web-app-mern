b95da3167d2644624cf424da884cfc23f5fcf443
Vadim Gavrilov <mr.midaw@gmail.com>
2022-04-05 14:07:10 +0300

    realize CRUD ticket funtionality

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getTickets, createTicket, } = require('../controllers/ticketController');
+const { getTickets, getTicket, createTicket, deleteTicket, updateTicket, } = require('../controllers/ticketController');
 
 const { protectAuth } = require('../middleware/authMiddleware');
 
 router.route('/').get(protectAuth, getTickets).post(protectAuth, createTicket);
-
+router.route('/:id').get(protectAuth, getTicket).delete(protectAuth, deleteTicket).put(protectAuth, updateTicket);
 
 module.exports = router;
